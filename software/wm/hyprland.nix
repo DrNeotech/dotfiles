1bfea77c8ed68615af5c6a85e07c2b5921ed3eee
DrNeotech <kaitlyn.oc.newton@gmail.com>
2025-07-10 22:51:02 +1000

    edit hypr

--- software/wm/hyprland.nix
+++ software/wm/hyprland.nix
@@ -4,10 +4,13 @@
 		./hyprland/hypr-config.nix
     ./hyprland/env.nix
   	];
-	programs = {
-        hyprland = {
-            enable = true;
-            xwayland.enable = true;
-        };
+    wayland.windowManager.hyprland = {
+        enable = true;
+        package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+        systemd.enable = true;
+        plugins = [
+            inputs.hypr-dynamic-cursors.packages.${pkgs.system}.hypr-dynamic-cursors
+        ] ++ lib.optional (settings.themeDetails.bordersPlusPlus)
+            inputs.hyprland-plugins.packages.${pkgs.system}.borders-plus-plus;
     };
 }
